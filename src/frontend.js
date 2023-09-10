21d5b490a112b0fd950780a12bc052d18b0a69ac
Lihao Lei <leilei199708@gmail.com>
2023-09-10 11:26:44 +0800

    adjust layout

--- src/frontend.js
+++ src/frontend.js
@@ -58,15 +58,21 @@ window.addEventListener( 'load', () => {
 	// Render the player
 	new Webamp( { ...options, ...milkdropOptions } ).renderWhenReady( container ).then( () => {
 		
-		// move webamp from body root to our container
+		// Move webamp from body root to our container
 		container.appendChild(document.getElementById( 'webamp' ));
-		
+
 		const player = document.getElementById( 'webamp' );
 
 		// Add is loaded class after artifical delay to reduce page jank
 		if ( player ) {
 			setTimeout( () => {
 				player.classList.add( 'is-loaded' );
+
+				document.getElementById("webamp").children[0].children[0].childNodes.forEach(elem => {
+					elem.style.transform = "";
+					elem.style.position = "";
+				})
+
 			}, 1000 );
 		}
 	} );
